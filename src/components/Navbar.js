9ff8b5ae98b7228acd0a9b5ff6cc6d56a21ca2f1
Chris <oaoxd0314@gmail.com>
2020-09-20 22:09:41 +0800

    css fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
 export default function navbar() {
   return (
     <div className="navbar">
       <section>
         <h1>Chris's blog</h1>
         <div className="path">
-          <h2>Home </h2>|<h2> About </h2>|<h2> Code Notes </h2>
+          <h2>
+            <Link to="/">Home</Link>
+          </h2>
+          |
+          <h2>
+            <Link to="/About">About</Link>
+          </h2>
+          |
+          <h2>
+            <Link to="/Notes">Code Notes</Link>
+          </h2>
         </div>
       </section>
     </div>
